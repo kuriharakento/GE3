57495cef8151b268418ad126362564ccfdb18291
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-07-04 12:48:22 +0900

    02_03確認課題

--- Input.cpp
+++ Input.cpp
@@ -40,9 +40,30 @@ void Input::Initialize(HINSTANCE hInstance, HWND hwnd)
 
 void Input::Update()
 {
+	//前回のキー入力を保存
+	memcpy(keyPre, key, sizeof(key));
+
 	//キーボードの情報の取得開始
 	keyboard->Acquire();
-	//全キーの入力情報を取得する
-	BYTE key[256] = {};
 	keyboard->GetDeviceState(sizeof(key), key);
 }
+
+bool Input::PushKey(BYTE keyNumber)
+{
+	//指定のキーを押していればtrueを返す
+	if(key[keyNumber])
+	{
+		return true;
+	}
+	return false;
+}
+
+bool Input::TriggerKey(BYTE keynumber)
+{
+	if(key[keynumber] && !keyPre[keynumber])
+	{
+		return true;
+	}
+	return false;
+}
+
--- Input.h
+++ Input.h
@@ -10,6 +10,7 @@ class Input
 public:
 	//namespace省略
 	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
+
 public:		//メンバ関数
 	//初期化
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
@@ -17,8 +18,25 @@ public:		//メンバ関数
 	//更新
 	void Update();
 
+public:		//キー状態取得関数
+	/// \brief キーの押下をチェック
+	/// \param keyNumber キー番号
+	/// \return 押されているか
+	bool PushKey(BYTE keyNumber);
+
+	/// \brief キーのトリガーをチェック
+	/// \param keynumber キー番号
+	/// \return トリガーか
+	bool TriggerKey(BYTE keynumber);
+
 private:
 	//キーボードデバイス生成
 	ComPtr<IDirectInputDevice8> keyboard;
+
+	//全キーの状態
+	BYTE key[256] = {};
+
+	//前回の全キーの状態
+	BYTE keyPre[256] = {};
 };
 
