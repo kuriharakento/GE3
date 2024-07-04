e1413c245da3e003b06bea79d9814dd13d864613
kuriharakento <146906405+kuriharakento@users.noreply.github.com>
2024-07-04 12:34:28 +0900

    02_02の課題をここでやってしまった

--- Input.h
+++ Input.h
@@ -1,8 +1,15 @@
 #pragma once
 #include <Windows.h>
+#include <wrl.h>
+#define DIRECTINPUT_VERSION	0x0800	//DirectInputのバージョン指定
+#include  <dinput.h>
+
 
 class Input
 {
+public:
+	//namespace省略
+	template <class T> using ComPtr = Microsoft::WRL::ComPtr<T>;
 public:		//メンバ関数
 	//初期化
 	void Initialize(HINSTANCE hInstance,HWND hwnd);
@@ -11,6 +18,7 @@ public:		//メンバ関数
 	void Update();
 
 private:
-
+	//キーボードデバイス生成
+	ComPtr<IDirectInputDevice8> keyboard;
 };
 
